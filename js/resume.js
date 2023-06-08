26e7f4a1e7705877f2d8ea1fb52d1766ae48967e
MSI\alylin <aly.chianing@gmail.com>
2023-06-08 14:25:56 +0800

    create resume page

--- js/resume.js
+++ js/resume.js
@@ -1,35 +1,36 @@
-// 获取 PDF 文件的 URL
+// 透過 pdf.js 渲染 pdf 檔在頁面上 (開始)
+// 獲取 PDF 文件的 URL
 const pdfUrl = "file/resume.pdf";
 
-// 获取容器元素
+// 獲取容器元素
 const container = document.getElementById("pdf-container");
 
-// 加载 PDF 文件
+// 加載 PDF 文件
 pdfjsLib.getDocument(pdfUrl).promise.then((pdf) => {
-  // 定义递归函数来处理每一页
+  // 定義遞迴來抓頁數
   function renderPage(pageNumber) {
     pdf.getPage(pageNumber).then((page) => {
       const scale = 1.5;
       const viewport = page.getViewport({ scale });
 
-      // 创建 Canvas 元素
+      // create Canvas 元素
       const canvas = document.createElement("canvas");
       canvas.className = "pdf-page";
       const context = canvas.getContext("2d");
       canvas.width = viewport.width;
       canvas.height = viewport.height;
 
-      // 渲染页面
+      // 渲染頁面
       page
         .render({
           canvasContext: context,
           viewport,
         })
         .promise.then(() => {
-          // 将 Canvas 元素添加到页面容器
+          // 將 Canvas 元素添加到容器
           container.appendChild(canvas);
 
-          // 递归处理下一页
+          // 處理下一页
           if (pageNumber < pdf.numPages) {
             renderPage(pageNumber + 1);
           }
@@ -37,6 +38,7 @@ pdfjsLib.getDocument(pdfUrl).promise.then((pdf) => {
     });
   }
 
-  // 从第一页开始递归处理
+  // 從第一頁開始遞歸處理
   renderPage(1);
 });
+// 透過 pdf.js 渲染 pdf 檔在頁面上 (結束)
